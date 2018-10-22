62b8debe9837aa86156ce9ba61ca276b36148426
Oleg Koretsky <test.radiokot@gmail.com>
2018-10-22 13:28:33 +0300

    Update byte-char extensions encoding and javadoc

--- src/main/kotlin/org/tokend/wallet/utils/ByteCharArray.kt
+++ src/main/kotlin/org/tokend/wallet/utils/ByteCharArray.kt
@@ -4,18 +4,24 @@ import java.nio.ByteBuffer
 import java.nio.CharBuffer
 import java.nio.charset.Charset
 
+/**
+ * Converts current char sequence to raw bytes representation by UTF-8.
+ */
 fun CharArray.toByteArray(): ByteArray {
     val charBuffer = CharBuffer.wrap(this)
-    val byteBuffer = Charset.defaultCharset().encode(charBuffer)
+    val byteBuffer = Charset.forName("UTF-8").encode(charBuffer)
     charBuffer.clear()
     val bytes = ByteArray(byteBuffer.remaining())
     byteBuffer.get(bytes).clear()
     return bytes
 }
 
+/**
+ * Converts current byte sequence to chars by UTF-8.
+ */
 fun ByteArray.toCharArray(): CharArray {
     val byteBuffer = ByteBuffer.wrap(this)
-    val charBuffer = Charset.defaultCharset().decode(byteBuffer)
+    val charBuffer = Charset.forName("UTF-8").decode(byteBuffer)
     byteBuffer.clear()
     val chars = CharArray(charBuffer.remaining())
     charBuffer.get(chars).clear()
