22f34398729723b6970bbd5db55e5d399b6b5700
Oleg Koretsky <test.radiokot@gmail.com>
2018-10-22 13:30:26 +0300

    Updated hashing util javadoc

--- src/main/kotlin/org/tokend/wallet/utils/Hashing.kt
+++ src/main/kotlin/org/tokend/wallet/utils/Hashing.kt
@@ -3,7 +3,14 @@ package org.tokend.wallet.utils
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
+/**
+ * Wraps [MessageDigest] calls for hashing algorithms.
+ */
 object Hashing {
+    /**
+     * @return SHA-256 hash of the given data.
+     */
+    @JvmStatic
     fun sha256(data: ByteArray): ByteArray {
         try {
             val digest = MessageDigest.getInstance("SHA-256")
