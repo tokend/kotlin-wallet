04f4f52c1ed1aece0aa7a7bf31e0c86ac53ad150
Oleg Koretsky <test.radiokot@gmail.com>
2018-12-18 16:36:24 +0200

    Added op extensions for SetFeesOp

---
+++ src/main/kotlin/org/tokend/wallet/xdr/op_extensions/DeleteFeeOp.kt
@@ -0,0 +1,21 @@
+package org.tokend.wallet.xdr.op_extensions
+
+import org.tokend.wallet.xdr.AccountType
+import org.tokend.wallet.xdr.FeeType
+import org.tokend.wallet.xdr.Int64
+
+class DeleteFeeOp
+@JvmOverloads
+constructor(
+        type: FeeType,
+        asset: String,
+        fixed: Int64,
+        percent: Int64,
+        upperBound: Int64,
+        lowerBound: Int64,
+        subtype: Int64 = 0L,
+        feeAsset: String? = null,
+        accountId: String? = null,
+        accountType: AccountType? = null
+) : SimpleSetFeesOp(true, type, asset, fixed, percent,
+        upperBound, lowerBound, subtype, feeAsset, accountId, accountType)
---
+++ src/main/kotlin/org/tokend/wallet/xdr/op_extensions/SimpleSetFeesOp.kt
@@ -0,0 +1,66 @@
+package org.tokend.wallet.xdr.op_extensions
+
+import org.tokend.wallet.PublicKeyFactory
+import org.tokend.wallet.utils.Hashing
+import org.tokend.wallet.xdr.*
+
+open class SimpleSetFeesOp : SetFeesOp {
+    @JvmOverloads
+    constructor(
+            isDelete: Boolean,
+            type: FeeType,
+            asset: String,
+            fixed: Int64,
+            percent: Int64,
+            upperBound: Int64,
+            lowerBound: Int64,
+            subtype: Int64 = 0L,
+            feeAsset: String? = null,
+            accountId: String? = null,
+            accountType: AccountType? = null
+
+    ) : super(
+            FeeEntry(
+                    type,
+                    asset,
+                    fixed,
+                    percent,
+                    if (accountId != null)
+                        PublicKeyFactory.fromAccountId(accountId)
+                    else
+                        null,
+                    accountType,
+                    subtype,
+                    lowerBound,
+                    upperBound,
+                    getHash(type, asset, subtype, accountId, accountType),
+                    if (feeAsset != null)
+                        FeeEntry.FeeEntryExt.CrossAssetFee(feeAsset)
+                    else
+                        FeeEntry.FeeEntryExt.EmptyVersion()
+            ),
+            isDelete,
+            SetFeesOpExt.EmptyVersion()
+    )
+
+    companion object {
+        @JvmStatic
+        fun getHash(type: FeeType, asset: String, subtype: Int64,
+                    accountId: String?, accountType: AccountType?): Hash {
+            var data =
+                    "type:${type.value}asset:${asset}subtype:$subtype"
+
+            if (accountId != null) {
+                data += "accountID:$accountId"
+            }
+
+            if (accountType != null) {
+                data += "accountType:${accountType.value}"
+            }
+
+            return Hash(
+                    Hashing.sha256(data.toByteArray())
+            )
+        }
+    }
+}
