dc51fb6d51114441247844b152bc0ba64c5f8a96
Oleg Koretsky <test.radiokot@gmail.com>
2019-11-26 17:35:25 +0200

    Added test for transaction hash

--- src/test/kotlin/org/tokend/wallet_test/TransactionTest.kt
+++ src/test/kotlin/org/tokend/wallet_test/TransactionTest.kt
@@ -6,6 +6,7 @@ import org.tokend.wallet.Account
 import org.tokend.wallet.NetworkParams
 import org.tokend.wallet.PublicKeyFactory
 import org.tokend.wallet.Transaction
+import org.tokend.wallet.utils.Base64
 import org.tokend.wallet.xdr.*
 import org.tokend.wallet.xdr.op_extensions.SimplePaymentOp
 
@@ -15,6 +16,35 @@ class TransactionTest {
 
     @Test
     fun encoding() {
+        val transaction = getSampleTransaction()
+
+        val expectedEnvelope = "AAAAAOqZBkGRIx3h+1qWebh2IUwpLHYaHDJHYlLzejKvy58+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAqAAAAAQAAAAtTYW1wbGUgdGV4dAAAAAABAAAAAAAAABcAAAAAaxoCTBacwprHg8fJzvPOfgE4trFmHuNq9/vyypIBHkcAAAAAAAAAAMMwGJJljz05EmGTr5w+tAOi1aQiMr1KIR6FduvL8njVAAAAAAAPQkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEVGVzdAAAAAAAAAAAAAAAAAAAAAHjQTipAAAAQF34oMJ+LK2Zu5FQIxbCsETYs5ELbzp4QsjS/5iu5rwxSiNtKBOGPwN43O57bMOEetTYdPWC+J2BKASM7eXVKQ0="
+        val envelope = transaction.getEnvelope().toBase64()
+        Assert.assertEquals(expectedEnvelope, envelope)
+    }
+
+    @Test
+    fun noOperations() {
+        try {
+            Transaction(
+                    NETWORK,
+                    PublicKeyFactory.fromAccountId(SOURCE_ACCOUNT_ID),
+                    emptyList())
+            Assert.fail("Transactions with no operations can't be allowed")
+        } catch (e: Exception) {
+        }
+    }
+
+    @Test
+    fun hash() {
+        val transaction = getSampleTransaction()
+
+        val expectedHash = "TcNNk7QSlWHviChZnnuwUp5tE6BxXL2BhFpWD6/4k3M="
+        val hash = Base64.encode(transaction.getHash()).toString(Charsets.UTF_8)
+        Assert.assertEquals(expectedHash, hash)
+    }
+
+    private fun getSampleTransaction(): Transaction {
         val sourceBalance = "BBVRUASMC2OMFGWHQPD4TTXTZZ7ACOFWWFTB5Y3K6757FSUSAEPEPXAS"
         val sourceAccountSeed = "SBEBZQIXHAZ3BZXOJEN6R57KMEDISGBIIP6LAVRCNDM4WZIQPHNYZICC".toCharArray()
         val destAccount = "GDBTAGESMWHT2OISMGJ27HB6WQB2FVNEEIZL2SRBD2CXN26L6J4NKDP2"
@@ -22,7 +52,7 @@ class TransactionTest {
 
         val paymentOp = SimplePaymentOp(
                 sourceBalanceId = sourceBalance,
-                destAccountId =destAccount,
+                destAccountId = destAccount,
                 amount = 1 * 1000000L,
                 feeData = PaymentFeeData(
                         Fee(0L, 0L, Fee.FeeExt.EmptyVersion()),
@@ -43,20 +73,6 @@ class TransactionTest {
         )
         transaction.addSignature(account)
 
-        val expectedEnvelope = "AAAAAOqZBkGRIx3h+1qWebh2IUwpLHYaHDJHYlLzejKvy58+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAqAAAAAQAAAAtTYW1wbGUgdGV4dAAAAAABAAAAAAAAABcAAAAAaxoCTBacwprHg8fJzvPOfgE4trFmHuNq9/vyypIBHkcAAAAAAAAAAMMwGJJljz05EmGTr5w+tAOi1aQiMr1KIR6FduvL8njVAAAAAAAPQkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEVGVzdAAAAAAAAAAAAAAAAAAAAAHjQTipAAAAQF34oMJ+LK2Zu5FQIxbCsETYs5ELbzp4QsjS/5iu5rwxSiNtKBOGPwN43O57bMOEetTYdPWC+J2BKASM7eXVKQ0="
-        val envelope = transaction.getEnvelope().toBase64()
-        Assert.assertEquals(expectedEnvelope, envelope)
-    }
-
-    @Test
-    fun noOperations() {
-        try {
-            Transaction(
-                    NETWORK,
-                    PublicKeyFactory.fromAccountId(SOURCE_ACCOUNT_ID),
-                    emptyList())
-            Assert.fail("Transactions with no operations can't be allowed")
-        } catch (e: Exception) {
-        }
+        return transaction
     }
 }
