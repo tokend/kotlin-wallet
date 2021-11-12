b4ea6b90d70b81e15be1e6fa512fd0d710d7d4fe
Oleg Koretsky <test.radiokot@gmail.com>
2021-11-12 18:01:52 +0200

    Fix unability to convert true UInt64 amounts from and to precised

--- src/main/kotlin/org/tokend/wallet/NetworkParams.kt
+++ src/main/kotlin/org/tokend/wallet/NetworkParams.kt
@@ -3,6 +3,7 @@ package org.tokend.wallet
 import org.tokend.wallet.utils.Hashing
 import java.io.Serializable
 import java.math.BigDecimal
+import java.math.BigInteger
 import java.math.MathContext
 import java.math.RoundingMode
 import java.util.*
@@ -10,7 +11,7 @@ import java.util.*
 /**
  * Holds network-specific parameters.
  */
-class NetworkParams: Serializable {
+class NetworkParams : Serializable {
     /**
      * Passphrase of the network
      */
@@ -48,13 +49,15 @@ class NetworkParams: Serializable {
 
     /**
      * @param passphrase network passphrase
-     * @param precision decimal places in amounts, [DEFAULT_PRECISION] by default
+     * @param precision decimal places in amounts, [DEFAULT_PRECISION] by default, [MAX_PRECISION] max
      * @param timeOffsetSeconds offset between device and server time in seconds, 0 by default
      */
     @JvmOverloads
     constructor(passphrase: String,
                 precision: Int = DEFAULT_PRECISION,
                 timeOffsetSeconds: Int = 0) {
+        require(precision <= MAX_PRECISION) { "Precision can't be bigger than $MAX_PRECISION" }
+
         this.passphrase = passphrase
         this.precision = precision
         this.precisionMultiplier = BigDecimal.TEN.pow(precision).longValueExact()
@@ -65,29 +68,48 @@ class NetworkParams: Serializable {
     /**
      * Converts given amount to network format.
      *
+     * @return UInt64 value in [Long], may result in negative Java value
+     * if the precised amount is bigger than 9223372036854775807
+     *
      * @see NetworkParams.precision
      */
     fun amountToPrecised(amount: BigDecimal): Long {
+        require(amount.signum() >= 0) { "Amount can't be negative" }
+
         return amount
                 .multiply(BigDecimal(precisionMultiplier))
                 .setScale(0, RoundingMode.DOWN)
-                .longValueExact()
+                .also { check(it <= MAX_PRECISED_AMOUNT) { "$it overflows UInt64" } }
+                .toLong()
     }
 
     /**
      * Converts given amount from network format to human-readable.
+     *
+     * @param amount UInt64 value in [Long],
+     * negative Java values are treated as bigger than 9223372036854775807
      */
     fun amountFromPrecised(amount: Long): BigDecimal {
-        return BigDecimal(amount)
+        val amountBytes = ByteArray(Long.SIZE_BYTES).apply {
+            for (byteI in 0 until size) {
+                set(byteI, amount.ushr((size - 1 - byteI) * 8).toByte())
+            }
+        }
+
+        return BigDecimal(BigInteger(1, amountBytes))
                 .divide(BigDecimal(precisionMultiplier), MathContext.DECIMAL128)
     }
 
     companion object {
+        private val MAX_PRECISED_AMOUNT = BigDecimal("18446744073709551615")
+
         const val serialVersionUID = 5677019745177892600L
+
+        const val MAX_PRECISION = 6
 
         /**
          * Default amount precision in TokenD.
          */
-        const val DEFAULT_PRECISION = 6
+        const val DEFAULT_PRECISION = MAX_PRECISION
     }
 }
--- src/test/kotlin/org/tokend/wallet_test/NetworkParams.kt
+++ src/test/kotlin/org/tokend/wallet_test/NetworkParams.kt
@@ -14,20 +14,65 @@ class NetworkParams {
     @Test
     fun amountConversion() {
         val precision = 6
-        val amount = BigDecimal("1.234567")
+        val amounts = listOf(
+                BigDecimal("1.234567"),
+                BigDecimal("18446744073709.551615"),
+                BigDecimal.ONE,
+                BigDecimal.TEN,
+                BigDecimal.ZERO,
+                BigDecimal("18446744073709.551")
+        )
         val networkParams = NetworkParams("Test phrase", precision)
 
-        val precisedAmount = networkParams.amountToPrecised(amount)
-        val dePrecisedAmount = networkParams.amountFromPrecised(precisedAmount)
+        amounts.forEach { amount ->
+            Assert.assertEquals(
+                    amount,
+                    networkParams.amountFromPrecised(networkParams.amountToPrecised(amount))
+            )
+        }
+    }
 
-        Assert.assertEquals(amount, dePrecisedAmount)
+    @Test
+    fun amountConversionZeroPrecision() {
+        val networkParams = NetworkParams("Test phrase", 0)
+        val amount = BigDecimal("18446744073709551615")
+
+        Assert.assertEquals(
+                amount,
+                networkParams.amountFromPrecised(networkParams.amountToPrecised(amount))
+        )
+
+        val amountToCut = BigDecimal("184467440.777")
+        val cutAmount = BigDecimal("184467440")
+
+        Assert.assertEquals(
+                cutAmount,
+                networkParams.amountFromPrecised(networkParams.amountToPrecised(amountToCut))
+        )
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun amountConversionNegative() {
+        NetworkParams("Test phrase", 0)
+                .amountToPrecised(BigDecimal("-1"))
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun amountConversionOverflow() {
+        NetworkParams("Test phrase", 0)
+                .amountToPrecised(BigDecimal("18446744073709551619"))
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun tooBigPrecision() {
+        NetworkParams("Test phrase", 8)
     }
 
     @Test
     fun timeCorrection() {
         val correction = 60
         val networkParams = NetworkParams("Test phrase",
-                NetworkParams.DEFAULT_PRECISION, correction)
+                NetworkParams.MAX_PRECISION, correction)
 
         val actual = Date().time / 1000
         val calculated = networkParams.nowTimestamp
@@ -37,7 +82,7 @@ class NetworkParams {
 
     @Test
     fun serialization() {
-        val networkParams = NetworkParams("Test phrase", 10, 42)
+        val networkParams = NetworkParams("Test phrase", 4, 42)
         val byteArrayOutputStream = ByteArrayOutputStream()
         ObjectOutputStream(byteArrayOutputStream).writeObject(networkParams)
         val inputStream = ByteArrayInputStream(byteArrayOutputStream.toByteArray())
